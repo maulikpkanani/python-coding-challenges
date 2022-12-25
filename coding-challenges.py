90adb40ab0e4c8b5ae39001bc9b01bc02f8ea416
Maulik Kanani <mauk@sitecore.net>
2022-12-25 18:20:02 -0500

    25-12-2022 practice

--- coding-challenges.py
+++ coding-challenges.py
@@ -29,4 +29,28 @@ def sum_numbers(n):
     else:
         return n + sum_numbers(n-1)
 =====================
+Write a function that converts hours into seconds.
+def how_many_seconds(hours):
+    return hours*60*60
+=====================
+Given a list of numbers, create a function which returns the list but with each element's index in the list added to itself. This means you add 0 to the number at index 0, add 1 to the number at index 1, etc...
+Examples
+add_indexes([0, 0, 0, 0, 0]) ➞ [0, 1, 2, 3, 4]
+add_indexes([1, 2, 3, 4, 5]) ➞ [1, 3, 5, 7, 9]
+add_indexes([5, 4, 3, 2, 1]) ➞ [5, 5, 5, 5, 5]
+
+def add_indexes(lst):
+    return [i+val for i, val in enumerate(lst)]
+========================
+Write a function that takes a list of elements and returns only the integers.
+Examples
+return_only_integer([9, 2, "space", "car", "lion", 16]) ➞ [9, 2, 16]
+return_only_integer(["hello", 81, "basketball", 123, "fox"]) ➞ [81, 123]
+return_only_integer([10, "121", 56, 20, "car", 3, "lion"]) ➞ [10, 56, 20, 3]
+return_only_integer(["String",  True,  3.3,  1]) ➞ [1]
+
+def return_only_integer(lst):
+    my_set = set([i for i in lst if type(i) == int])
+    return list(my_set)
+	
 
