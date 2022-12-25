78ece451a7cb695888b58397cc45985672a741ff
Maulik Kanani <mauk@sitecore.net>
2022-12-24 23:35:43 -0500

    24-12-2022-practice

--- coding-challenges.py
+++ coding-challenges.py
@@ -2,7 +2,6 @@ https://edabit.com/challenges/python3
 
 1.Flip the Boolean
 Create a function that reverses a boolean value and returns the string "boolean expected" if another variable type is given.
-
 Examples
 reverse(True) ➞ False
 reverse(False) ➞ True
@@ -17,4 +16,17 @@ def reverse(arg):
     else:
         return False
 =====================
-2.
+2.Write a function that finds the sum of the first n natural numbers. Make your function recursive.
+Examples
+sum_numbers(5) ➞ 15
+# 1 + 2 + 3 + 4 + 5 = 15
+sum_numbers(1) ➞ 1
+sum_numbers(12) ➞ 78
+
+def sum_numbers(n):
+    if n == 1:
+        return 1
+    else:
+        return n + sum_numbers(n-1)
+=====================
+
