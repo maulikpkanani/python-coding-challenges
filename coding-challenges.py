b365aedb12b730ef8e98bc956d3dfbcc9cb18e7a
Maulik Kanani <mauk@sitecore.net>
2022-12-28 04:13:20 -0500

    28-12-2022-practice

--- coding-challenges.py
+++ coding-challenges.py
@@ -1,6 +1,6 @@
 https://edabit.com/challenges/python3
 
-1.Flip the Boolean
+Flip the Boolean
 Create a function that reverses a boolean value and returns the string "boolean expected" if another variable type is given.
 Examples
 reverse(True) ➞ False
@@ -16,7 +16,7 @@ def reverse(arg):
     else:
         return False
 =====================
-2.Write a function that finds the sum of the first n natural numbers. Make your function recursive.
+Write a function that finds the sum of the first n natural numbers. Make your function recursive.
 Examples
 sum_numbers(5) ➞ 15
 # 1 + 2 + 3 + 4 + 5 = 15
@@ -52,5 +52,102 @@ return_only_integer(["String",  True,  3.3,  1]) ➞ [1]
 def return_only_integer(lst):
     my_set = set([i for i in lst if type(i) == int])
     return list(my_set)
-	
+=======================
+Create a function that squares every digit of a number.
+Examples
+square_digits(9119) ➞ 811181
+square_digits(2483) ➞ 416649
+square_digits(3212) ➞ 9414
+
+def square_digits(n):
+    lst =(str(n))
+    estr = ''
+    for i in lst:
+        estr += str(int(i)**2)
+    return int(estr)
+
+def square_digits(n):
+    return int(''.join(str(int(i)**2) for i in str(n)))
+======================
+Write a function that stutters a word as if someone is struggling to read it. The first two letters are repeated twice with an ellipsis ... and space after each, and then the word is pronounced with a question mark ?.
+Examples
+stutter("incredible") ➞ "in... in... incredible?"
+stutter("enthusiastic") ➞ "en... en... enthusiastic?"
+
+def stutter(word):
+    return f"{word[:2]}.. {word[:2]}.. {word}?"
+    #return word[:2]+"... "+word[:2]+"... "+word+"?"
+======================
+Create a function that takes a number as input and returns True if the sum of its digits has the same parity as the entire number. Otherwise, return False.
+Examples
+parity_analysis(243) ➞ True
+# 243 is odd and so is 9 (2 + 4 + 3)
+parity_analysis(12) ➞ False
+# 12 is even but 3 is odd (1 + 2)
+parity_analysis(3) ➞ True
+# 3 is odd and 3 is odd and 3 is odd (3)
+
+def parity_analysis(num):
+    digit_sum = sum(map(int, str(num)))
+    return num %2 == digit_sum %2
+========================
+Create a function which concatenates the number 7 to the end of every chord in a list. Ignore all chords which already end with 7.
+Examples
+jazzify(["G", "F", "C"]) ➞ ["G7", "F7", "C7"]
+jazzify(["Dm", "G", "E", "A"]) ➞ ["Dm7", "G7", "E7", "A7"]
+jazzify(["F7", "E7", "A7", "Ab7", "Gm7", "C7"]) ➞ ["F7", "E7", "A7", "Ab7", "Gm7", "C7"]
+jazzify([]) ➞ []
+
+def jazzify(lst):
+    final_lst = []
+    if len(lst) != 0:
+        for i in lst:
+            if i.endswith('7'):
+                final_lst.append(i)
+            else:
+                final_lst.append(i+'7')
+        return final_lst
+    else:
+        return []
+def jazzify(lst):
+    if len(lst) != 0:
+        return [i if i.endswith('7') else i+'7' for i in lst]
+    else:
+        return []
+==============================
+Create a function that ends the first word of a phrase with "ed", essentially verbifying a noun.
+Examples
+verbify("cheese burger") ➞ "cheesed burger"
+verbify("salt water") ➞ "salted water"
+verbify("orange juice") ➞ "oranged juice"
+verbify("shredded cheese") ➞ "shredded cheese"
+
+def verbify(txt):
+    strlst = txt.split( )
+    if strlst[0].endswith('ed'):
+        #return f"{strlst[0]} {strlst[1]}"
+        return "{} {}".format(strlst[0],strlst[1])
+    elif strlst[0].endswith('e'):
+        #return f"{strlst[0]+'d'} {strlst[1]}"
+        return "{} {}".format(strlst[0]+'d',strlst[1])
+    else:
+        #return f"{strlst[0]+'ed'} {strlst[1]}"
+         return "{} {}".format(strlst[0]+'ed',strlst[1])
+========================
+Given a list of numbers, return a list which contains all the even numbers in the original list, which also have even indices.
+Examples
+get_only_evens([1, 3, 2, 6, 4, 8]) ➞ [2, 4]
+get_only_evens([0, 1, 2, 3, 4]) ➞ [0, 2, 4]
+get_only_evens([1, 2, 3, 4, 5]) ➞ []
+
+def get_only_evens(nums):
+    lst = []
+    for i, num in enumerate(nums):
+        if num%2 == i%2 == 0:
+            lst.append(num)
+        else:
+            lst
+    return lst
+def get_only_evens(nums):
+	return [i for i in nums[::2] if i%2==0]
 
